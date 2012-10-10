4cf10d0b82c2663e52dd50218d766e4b85eda18b
Chris Rault <Chris@Chris-Raults-iMac.local>
2012-10-10 18:57:47 +0200

    Re-applied some changes I forgot to commit :)

--- block.php
+++ block.php
@@ -93,7 +93,7 @@ class HeadwaySubtextBlock extends HeadwayBlockAPI {
 			echo $assignment_notice;
 		}
 		
-		echo '<div class="block-type-navigation"><div class="' . $nav_classes . '">';
+		echo '<div id="nav-' . $block['id'] . '" class="block-type-navigation"><div class="' . $nav_classes . '">';
 				
 				if (self::is_menu_assigned($block) == true) {
 					$nav_menu_args = array(
@@ -145,6 +145,7 @@ class HeadwaySubtextBlock extends HeadwayBlockAPI {
 		$css            			= '';
 
 		if (self::is_menu_assigned($block) == true) {
+			$css .= '#block-'.$block_id.' .nav-horizontal ul.menu { margin: 0; padding: 0; }';
 			$css .= '#block-'.$block_id.' .nav-horizontal ul.menu > li > a { height: 55px; line-height: 1; padding-top: 13px; }';
 			$css .= '#block-'.$block_id.' ul li a span.subtext { display: block; line-height: 1; margin-top: ' . $subtext_margin . 'px; }';
 		} else {
@@ -229,70 +230,99 @@ class HeadwaySubtextBlock extends HeadwayBlockAPI {
 	
 	
 	function setup_elements() {
+		$subtext = HeadwayBlocksData::get_blocks_by_type('hwr-subtext');
+		$return = '';
 
-		$this->register_block_element(array(
-			'id' => 'menu-item',
-			'name' => 'Menu Item',
-			'selector' => 'ul.menu li a',
-			'properties' => array('fonts' => array('font-family', 'font-size', 'color', 'font-styling', 'capitalization', 'letter-spacing', 'text-decoration'), 'background', 'borders', 'padding', 'rounded-corners', 'box-shadow', 'text-shadow'),
-			'states' => array(
-				'Selected' => 'ul.menu li.current_page_item > a, 
-							   ul.menu li.current_page_parent > a, 
-							   ul.menu li.current_page_ancestor > a, 
-							   ul.menu li.current_page_item > a:hover, 
-							   ul.menu li.current_page_parent > a:hover, 
-							   ul.menu li.current_page_ancestor > a:hover', 
-				'Hover' => 'ul.menu li > a:hover', 
-				'Clicked' => 'ul.menu li > a:active'
-			)
-		));
+		if ( !isset($subtext) || !is_array($subtext) )
+			return $return;
 		
-		$this->register_block_element(array(
-			'id' => 'item-subtext',
-			'name' => 'Menu Item Subtext',
-			'selector' => 'ul.menu li a span.subtext',
-			'properties' => array('fonts' => array('font-family', 'font-size', 'color', 'font-styling', 'capitalization', 'letter-spacing', 'text-decoration'), 'background', 'borders', 'rounded-corners', 'box-shadow', 'text-shadow'),
-			'states' => array(
-				'Selected' => 'ul.menu li a:focus span.subtext', 
-				'Hover' => 'ul.menu li a:hover span.subtext', 
-				'Clicked' => 'ul.menu li a:active span.subtext'
-			)
-		));
-		
-		$this->register_block_element(array(
-			'id' => 'sub-nav-menu',
-			'name' => 'Sub Menu',
-			'selector' => 'ul.sub-menu',
-			'properties' => array('background', 'borders', 'padding', 'rounded-corners', 'box-shadow')
-		));
-		$this->register_block_element(array(
-			'id' => 'sub-menu-item',
-			'name' => 'Sub Menu Item',
-			'selector' => 'ul.sub-menu li > a',
-			'properties' => array(
-				'fonts' => array('font-family', 'font-size', 'color', 'font-styling', 'capitalization', 'letter-spacing', 'text-decoration'), 
-				'background', 
-				'borders', 
-				'padding', 
-				'rounded-corners', 
-				'box-shadow', 
-				'text-shadow'
-			),
-			'states' => array(
-				'Selected' => '
-					ul.sub-menu li.current_page_item > a, 
-					ul.sub-menu li.current_page_parent > a, 
-					ul.sub-menu li.current_page_ancestor > a, 
-					ul.sub-menu li.current_page_item > a:hover, 
-					ul.sub-menu li.current_page_parent > a:hover, 
-					ul.sub-menu li.current_page_ancestor > a:hover
-				', 
-				'Hover' => 'ul.sub-menu li > a:hover', 
-				'Clicked' => 'ul.sub-menu li > a:active'
-			),
-			'inherit-location' => 'block-navigation-menu-item'
-		));
+		foreach ($subtext as $block_id => $layout_id) {
 
+
+			$this->register_block_element(array(
+				'id' => 'menu-' . $block_id,
+				'name' => 'Menu',
+				'selector' => '#block-' . $block_id . ' ul.menu',
+				'properties' => array('background', 'borders', 'padding', 'rounded-corners', 'box-shadow')
+			));
+			$this->register_block_element(array(
+				'id' => 'menu-item-' . $block_id,
+				'name' => 'Menu Item',
+				'selector' => '#nav-' . $block_id . ' ul.menu li a',
+				'properties' => array('fonts' => array('font-family', 'font-size', 'color', 'font-styling', 'capitalization', 'letter-spacing', 'text-decoration'), 'background', 'borders', 'padding', 'rounded-corners', 'box-shadow', 'text-shadow'),
+				'states' => array(
+					'Selected' => 'ul.menu li.current_page_item > a, 
+								   ul.menu li.current_page_parent > a, 
+								   ul.menu li.current_page_ancestor > a, 
+								   ul.menu li.current_page_item > a:hover, 
+								   ul.menu li.current_page_parent > a:hover, 
+								   ul.menu li.current_page_ancestor > a:hover', 
+					'Hover' => 'ul.menu li > a:hover', 
+					'Clicked' => 'ul.menu li > a:active'
+				)
+			));
+			$this->register_block_element(array(
+				'id' => 'menu-item-active-' . $block_id,
+				'name' => 'Active Menu Item',
+				'selector' => '#nav-' . $block_id . ' ul.menu li.current_page_item a',
+				'properties' => array('fonts' => array('font-family', 'font-size', 'color', 'font-styling', 'capitalization', 'letter-spacing', 'text-decoration'), 'background', 'borders', 'padding', 'rounded-corners', 'box-shadow', 'text-shadow'),
+				'states' => array(
+					'Selected' => 'ul.menu li.current_page_item > a, 
+								   ul.menu li.current_page_parent > a, 
+								   ul.menu li.current_page_ancestor > a, 
+								   ul.menu li.current_page_item > a:hover, 
+								   ul.menu li.current_page_parent > a:hover, 
+								   ul.menu li.current_page_ancestor > a:hover', 
+					'Hover' => 'ul.menu li > a:hover', 
+					'Clicked' => 'ul.menu li > a:active'
+				)
+			));
+			$this->register_block_element(array(
+				'id' => 'item-subtext-' . $block_id,
+				'name' => 'Menu Item Subtext',
+				'selector' => '#nav-' . $block_id . ' ul.menu li a span.subtext',
+				'properties' => array('fonts' => array('font-family', 'font-size', 'color', 'font-styling', 'capitalization', 'letter-spacing', 'text-decoration'), 'background', 'borders', 'rounded-corners', 'box-shadow', 'text-shadow'),
+				'states' => array(
+					'Selected' => 'ul.menu li a:focus span.subtext', 
+					'Hover' => 'ul.menu li a:hover span.subtext', 
+					'Clicked' => 'ul.menu li a:active span.subtext'
+				)
+			));
+			$this->register_block_element(array(
+				'id' => 'sub-nav-menu-' . $block_id,
+				'name' => 'Sub Menu',
+				'selector' => '#nav-' . $block_id . ' ul.sub-menu',
+				'properties' => array('background', 'borders', 'padding', 'rounded-corners', 'box-shadow')
+			));
+			$this->register_block_element(array(
+				'id' => 'sub-menu-item-' . $block_id,
+				'name' => 'Sub Menu Item',
+				'selector' => '#nav-' . $block_id . ' ul.sub-menu li > a',
+				'properties' => array(
+					'fonts' => array('font-family', 'font-size', 'color', 'font-styling', 'capitalization', 'letter-spacing', 'text-decoration'), 
+					'background', 
+					'borders', 
+					'padding', 
+					'rounded-corners', 
+					'box-shadow', 
+					'text-shadow'
+				),
+				'states' => array(
+					'Selected' => '
+						#block-' . $block_id . ' ul.sub-menu li.current_page_item > a, 
+						#block-' . $block_id . ' ul.sub-menu li.current_page_parent > a, 
+						#block-' . $block_id . ' ul.sub-menu li.current_page_ancestor > a, 
+						#block-' . $block_id . ' ul.sub-menu li.current_page_item > a:hover, 
+						#block-' . $block_id . ' ul.sub-menu li.current_page_parent > a:hover, 
+						#block-' . $block_id . ' ul.sub-menu li.current_page_ancestor > a:hover
+					', 
+					'Hover' => '#block-' . $block_id . ' ul.sub-menu li > a:hover', 
+					'Selected' => 'ul.menu li.current_page_item a', 
+					'Clicked' => '#block-' . $block_id . ' ul.sub-menu li > a:active'
+				),
+				'inherit-location' => 'block-navigation-menu-item'
+			));
+		}
 	}
 	
 	function set_options_defaults() {
@@ -308,13 +338,33 @@ class HeadwaySubtextBlock extends HeadwayBlockAPI {
 		$new_headway_default_element_data = array();
 		
 		foreach ($subtext_blocks as $block_id => $layout_id) {
-			
-			$new_headway_default_element_data['block-hwr-subtext-item-subtext'] = array(
+			$new_headway_default_element_data['block-hwr-subtext-menu-item-' . $block_id] = array(
+				'properties' => array(
+					'font-size' => '17',
+					'capitalization' => 'none',
+					'text-decoration' => 'none',
+					'color' => '555555'
+				),
+				'special-element-state' => array(
+					'selected' => array(
+						'color' => 'C25B00'
+					),
+					'hover' => array(
+						'color' => 'C25B00'
+					)
+				)
+			);
+			$new_headway_default_element_data['block-hwr-subtext-menu-item-active-' . $block_id] = array(
+				'properties' => array(
+					'color' => 'C25B00'
+				)
+			);
+			$new_headway_default_element_data['block-hwr-subtext-item-subtext-' . $block_id] = array(
 				'properties' => array(
 					'font-size' => '12',
 					'color' => '999999'
 				)
-			);	
+			);
 		}
 		$headway_default_element_data = array_merge($headway_default_element_data, $new_headway_default_element_data);
 	}
